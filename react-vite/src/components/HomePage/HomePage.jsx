d6655ad73de18a206953649ef1d07bb594a27e5d
Kai <fengkai0129@gmail.com>
2024-06-03 14:41:25 -0700

    home page design

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,20 @@
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import {IoIosArrowForward} from 'react-icons/io'
 import './HomePage.css';
 
 const HomePage = () => {
      return (
-     <div className='home-page-container'>
-          <h1></h1>
-          <p>Welcome to the MayTime!</p>
+     <div className="home-page-container">
+     <div className="home-page-text-title">
+        <h1 style={{fontFamily: 'rockwell'}}>MAYTIME</h1>
+        <div className="home-page-text">
+        {/* <NavLink to={'/pin'} style={{textDecoration:'none', color:'#000000'}}> */}
+        <h1>Unleash Your</h1>
+        <h2 style={{paddingLeft: '20px', marginTop: '-20px'}}>Creative Exploration.</h2>
+        {/* </NavLink> */}
+        </div>
+     </div>
+
      </div>
      );
 };
