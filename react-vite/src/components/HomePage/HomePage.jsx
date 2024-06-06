9049c861de0e9a8e077de335657db29ff048206b
Kai <fengkai0129@gmail.com>
2024-06-06 08:42:18 -0700

    background Color

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
      return (
      <div className="home-page-container">
      <div className="home-page-text-title">
-        <h1 style={{fontFamily: 'rockwell'}}>MAYTIME</h1>
+        <h1 style={{fontFamily: 'rockwell', letterSpacing: '5px'}}>MAYT<span style={{fontFamily: 'rockwell',color:'#ff2f00'}}>I</span>ME</h1>
         <div className="home-page-text">
         {/* <NavLink to={'/pin'} style={{textDecoration:'none', color:'#000000'}}> */}
         <h1>Unleash Your</h1>
