5df1293aef7e6e7e5c72fbc0eddcc113fccc3d37
Kai <fengkai0129@gmail.com>
2024-06-06 22:47:51 -0700

    home page

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -4,77 +4,63 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-      <div className="nav-bar-container">
-          <div className="nav-bar-left-logo">
-            <NavLink to="/">
-                  <img src='/public/Maytime logo FinalWhite.png'
-                        alt='Logo'
-                        className='nav-bar-left-logo'
-                  />
-            </NavLink>
-          </div>
-          <div className="nav-bar-left-links">
-              <NavLink
-                to="/"
-                className="nav-bar-button"
-                style={{ textDecoration: "none" }}
-              >
-                Home
-              </NavLink>
-              {/* <NavLink></NavLink> */}
+    <div className="nav-bar-container">
+      <div className="nav-bar-left-logo">
+        <NavLink to="/">
+          <img
+            src="/public/Maytime logo FinalWhite.png"
+            alt="Logo"
+            className="nav-bar-left-logo"
+          />
+        </NavLink>
+      </div>
+      <div className="nav-bar-left">
+        <div className="nav-bar-left-links">
+          <NavLink
+            to="/"
+            className="nav-bar-button"
+            style={{ textDecoration: "none" }}
+          >
+            Home
+          </NavLink>
+          {/* <NavLink></NavLink> */}
 
-              <NavLink
-                to="/"
-                style={{ textDecoration: "none" }}
-              >
-                <div
-                  className="nav-bar-button"
-                  // onClick={() => window.alert("Feature coming soon")}
-                  style={{ cursor: "pointer" }}
-                >
-                  Photos
-                </div>
-              </NavLink>
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <div
+              className="nav-bar-button"
+              // onClick={() => window.alert("Feature coming soon")}
+              style={{ cursor: "pointer" }}
+            >
+              Photos
+            </div>
+          </NavLink>
 
-              <NavLink
-                to="/"
-                style={{ textDecoration: "none" }}
-              >
-                <div
-                  className="nav-bar-button"
-                  // onClick={() => window.alert("Feature coming soon")}
-                  style={{ cursor: "pointer" }}
-                >
-                  About
-                </div>
-              </NavLink>
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <div
+              className="nav-bar-button"
+              // onClick={() => window.alert("Feature coming soon")}
+              style={{ cursor: "pointer" }}
+            >
+              About
+            </div>
+          </NavLink>
 
-              <NavLink
-                to="/"
-                style={{ textDecoration: "none" }}
-              >
-                <div
-                  className="nav-bar-button"
-                  // onClick={() => window.alert("Feature coming soon")}
-                  style={{ cursor: "pointer" }}
-                >
-                  Contact Us
-                </div>
-              </NavLink>
-
-              {/* <NavLink
-                to="/pin-creation-tool"
-                style={{ textDecoration: "none" }}
-              >
-                <div className="nav-bar-button">Create</div>
-              </NavLink> */}
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <div
+              className="nav-bar-button"
+              // onClick={() => window.alert("Feature coming soon")}
+              style={{ cursor: "pointer" }}
+            >
+              Contact Us
+            </div>
+          </NavLink>
         </div>
-
 
         <div className="nav-bar-right-loginButton">
           <ProfileButton />
         </div>
       </div>
+    </div>
   );
 }
 
