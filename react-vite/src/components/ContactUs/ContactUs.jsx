c19959b581c5db017926049615f254bf56de696e
Kai <fengkai0129@gmail.com>
2024-06-09 18:40:48 -0700

    ContactUs Page

--- react-vite/src/components/ContactUs/ContactUs.jsx
+++ react-vite/src/components/ContactUs/ContactUs.jsx
@@ -1,9 +1,65 @@
+import React, { useState } from "react";
+
 const ContactUs = () => {
-     return (
-          <div>
-               <h1 style={{margin:'100px 40px', color:'#ff6a00', width:'fit-content'}}>Contact Us</h1>
-          </div>
-     );
-     }
+  const [formData, setFromData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFromData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h1
+        style={{ margin: "100px 40px", color: "#ff6a00", width: "fit-content" }}
+      >
+        Contact Us
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div style={{ color: "white", margin: "40px" }}>
+          <h2>Name</h2>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Enter your name"
+          />
+          <h2>Email</h2>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Enter your email"
+          />
+
+          <h2>Message</h2>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Enter your message"
+          />
+        </div>
+        <button type="submit" style={{ color: "#ff6a00", margin: "40px" }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default ContactUs;
