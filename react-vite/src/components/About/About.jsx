48484e310bfa4abaed536b1a44b2f8a9055da915
Kai <fengkai0129@gmail.com>
2024-06-17 22:39:23 -0700

    about us

--- react-vite/src/components/About/About.jsx
+++ react-vite/src/components/About/About.jsx
@@ -1,34 +1,44 @@
 import "./About.css";
 
 const About = () => {
+  const handlePDFButtonClick = () => {
+    window.open("/path/to/the/file.pdf", "_blank");
+  };
+  return (
+    <div className="contact-us-page-container">
+      <h1
+        style={{
+          fontWeight: "bold",
+          backgroundColor: "#ffffff00",
+          color: "#ff6a00",
+          width: "fit-content",
+        }}
+      >
+        <span style={{ fontWeight: "bold" }}>ABOUT</span>{" "}
+        <span style={{ fontWeight: "lighter" }}>US</span>
+      </h1>
 
-     const handlePDFButtonClick = () => {
-          window.open("/path/to/the/file.pdf", "_blank");
-        };
-     return (
-          <div className="contact-us-page-container">
-               <h1 style={{fontWeight:'bold', backgroundColor:'#ffffff00', color:'#ff6a00', width:'fit-content'}}>
-                    <span style={{fontWeight:'bold'}}>ABOUT</span> <span style={{fontWeight:'lighter'}}>US</span>
-               </h1>
-
-               <div className="contact-us-page-button">
-                    <button
-                         type="button"
-                         onClick={handlePDFButtonClick}
-                         style={{
-                           color: "#ff6a00",
-                           borderWidth: "1px",
-                           borderColor: "#ff6a00",
-                           borderRadius: "10px",
-                           padding: "5px 10px",
-                           cursor: "pointer",
-                           backgroundColor: "transparent",
-                           borderStyle: "solid",
-                         }}
-                    > PRICE</button>
-               </div>
-          </div>
-     )
-     }
+      <div className="contact-us-page-button">
+        <button
+          type="button"
+          onClick={handlePDFButtonClick}
+          style={{
+            color: "#ff6a00",
+            borderWidth: "1px",
+            borderColor: "#ff6a00",
+            borderRadius: "10px",
+            padding: "5px 10px",
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            borderStyle: "solid",
+          }}
+        >
+          {" "}
+          PRICE
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default About;
