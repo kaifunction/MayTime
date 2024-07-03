0a4b93217adb85fcd94ad85f446bfe71c3689f4a
Kai <fengkai0129@gmail.com>
2024-07-03 09:01:51 -0700

    background img

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -1,43 +1,38 @@
 // import { NavLink } from 'react-router-dom';
 // import {IoIosArrowForward} from 'react-icons/io'
-import { NavLink } from 'react-router-dom';
-import './HomePage.css';
+import { NavLink } from "react-router-dom";
+import "./HomePage.css";
 
 const HomePage = () => {
-     return (
-     <div className="home-page-container">
-     <div className="home-page-text-title">
-        <h1 style={{fontFamily: 'rockwell', letterSpacing: '5px', color:'#c2c2c2'}}><span style={{fontFamily: 'rockwell',color:'#ff6a00 '}}>M</span>AY<span style={{fontFamily: 'rockwell',color:'#ff6a00'}}>T</span>IME</h1>
-        {/* <h1 style={{fontFamily: 'rockwell', letterSpacing: '9px', color:'#c2c2c2'}}>MAYTIME</h1> */}
-
-        {/* <img src='/public/homepage.jpg' alt='homepage' className='homepage-image' /> */}
-        <div className='text-links'>
-         <div className="home-page-text">
-        {/* <NavLink to={'/pin'} style={{textDecoration:'none', color:'#000000'}}> */}
-
-        <h1>Unleash Your</h1>
-        <h2 style={{paddingLeft: '20px', marginTop: '-20px', color:'#c2c2c2'}}>Creative Exploration.</h2>
-
-        {/* </NavLink> */}
-        </div>
-        <div className='socialmedialinks'>
-         <NavLink to={'https://www.xiaohongshu.com/user/profile/650f8469000000001200706a'} target='_blank'>
-         <img src='/public/xiaohongshuLOGO.png' alt='xiaohongshu' className='xiaohongshu-logo'  />
-         </NavLink>
-         <NavLink to={'https://www.instagram.com/maytimejewelry/'} target='_blank'>
-         <img src='/public/instagram.png' alt='instagram' className='instagram-logo' />
-         </NavLink>
-         <NavLink to={'https://www.facebook.com/maytimejewelry'} target='_blank'>
-         <img src='/wechat.svg' alt='wechat' className='wechat-logo' />
-         </NavLink>
-        </div>
-        </div>
-
-
-     </div>
-     </div>
-     );
+  return (
+    <div className="home-page-container">
+      <div className="overlay">
+                <div className="home-page-text-title">
+                    <h1 style={{ fontFamily: 'rockwell', letterSpacing: '5px', color: '#c2c2c2', backgroundColor:'rgba(0,0,0,0)' }}>
+                        <span style={{ fontFamily: 'rockwell', color: '#ff6a00 ' , backgroundColor:'rgba(0,0,0,0)'}}>M</span>AY
+                        <span style={{ fontFamily: 'rockwell', color: '#ff6a00', backgroundColor:'rgba(0,0,0,0)' }}>T</span>IME
+                    </h1>
+                    <div className='text-links' style={{backgroundColor:'rgba(0,0,0,0)'}}>
+                        <div className="home-page-text" style={{backgroundColor:'rgba(0,0,0,0)'}}>
+                            <h1>Unleash Your</h1>
+                            <h2 style={{ paddingLeft: '20px', marginTop: '-20px', color: '#c2c2c2',backgroundColor:'rgba(0,0,0,0)' }}>Creative Exploration.</h2>
+                        </div>
+                        <div className='socialmedialinks' style={{backgroundColor:'rgba(0,0,0,0)'}}>
+                            <NavLink to={'https://www.xiaohongshu.com/user/profile/650f8469000000001200706a'} target='_blank'>
+                                <img src='/public/xiaohongshuLOGO.png' alt='xiaohongshu' className='xiaohongshu-logo' />
+                            </NavLink>
+                            <NavLink to={'https://www.instagram.com/maytimejewelry/'} target='_blank'>
+                                <img src='/public/instagram.png' alt='instagram' className='instagram-logo' />
+                            </NavLink>
+                            <NavLink to={'https://www.facebook.com/maytimejewelry'} target='_blank'>
+                                <img src='/wechat.svg' alt='wechat' className='wechat-logo' />
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  );
 };
-
 
 export default HomePage;
