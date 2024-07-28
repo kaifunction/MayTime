d88d577a73b30f6d9507091d26df2cb9672a0f96
Kai <fengkai0129@gmail.com>
2024-07-28 15:30:20 -0700

    photos

--- react-vite/src/components/Photos/Photo.jsx
+++ react-vite/src/components/Photos/Photo.jsx
@@ -5,6 +5,9 @@ const Photos = () => {
     <div className="photo-container">
       <div className="photo-overlay">
         <img/>
+        <img/>
+        <img/>
+        <img/>
       </div>
       <h1 style={{ color:'#ff6a00', width:'fit-content'}}><span style={{fontWeight:'bold'}}>PHOTO</span><span style={{fontWeight:'lighter'}}>S</span></h1>
     </div>
