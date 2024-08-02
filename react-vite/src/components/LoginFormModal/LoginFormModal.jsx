f5196a977f64ff6f6b3b72f6b82ba9772ec36c63
Kai <fengkai0129@gmail.com>
2024-08-02 10:24:57 -0700

    login modal

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -8,11 +8,39 @@ function LoginFormModal() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
   const { closeModal } = useModal();
+
+
+  const validateEP = () => {
+    let isValid = true;
+    let tempErrors = { email: "", password: "" };
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+      tempErrors.email = "Email is required.";
+      isValid = false;
+    } else if (!emailRegex.test(email)) {
+      tempErrors.email = "Email is invalid.";
+      isValid = false;
+    }
+
+    if (!password) {
+      tempErrors.password = "Password is required.";
+      isValid = false;
+    }
+
+    setErrors(tempErrors);
+    return isValid;
+
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validateEP()) return;
 
     const serverResponse = await dispatch(
       thunkLogin({
@@ -28,35 +56,36 @@ function LoginFormModal() {
     }
   };
 
+
   return (
-    <>
-      <h1 style={{color:'#ff6a00', backgroundColor:'black',padding:'20px', margin:'0px'}}>Log In</h1>
+    <div className="login-container">
+      <h1 style={{color:'#ff6a00', backgroundColor:'black',padding:'20px 20px 20px 0', margin:'0px'}}>Log In</h1>
       <form onSubmit={handleSubmit}>
-        <label style={{color:'#ff6a00'}}>
-          Email
+        <label style={{color:'#ff6a00', marginTop:'60px'}}>
+          Email :
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            style={{backgroundColor: 'black', color: 'white'}}
+            style={{backgroundColor: 'black', color: 'white', marginLeft:'20px', width:'200px'}}
           />
         </label>
-        {errors.email && <p style={{color:'#f94880',fontSize:'12px'}}>{errors.email}</p>}
+        {errors.email && <p className="error">{errors.email}</p>}
         <label style={{color:'#ff6a00'}}>
-          Password
+          Password :
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            style={{backgroundColor: 'black', color: 'white'}}
+            style={{backgroundColor: 'black', color: 'white', marginLeft:'20px', width:'168px'}}
           />
         </label>
-        {errors.password && <p style={{color:'#f94880',fontSize:'12px'}}>{errors.password}</p>}
+        {errors.password && <p className="error">{errors.password}</p>}
         <button type="submit" style={{color:'black', backgroundColor:'#ff6a00', cursor:'pointer', padding:'10px', borderRadius:'10px'}}>Log In</button>
       </form>
-    </>
+    </div>
   );
 }
 
