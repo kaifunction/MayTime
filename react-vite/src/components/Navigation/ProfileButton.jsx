cc06bdda453a9ac6e4d4152816eed45b5b7d9b24
Kai <fengkai0129@gmail.com>
2024-09-10 09:34:55 -0700

    spacr

--- react-vite/src/components/Navigation/ProfileButton.jsx
+++ react-vite/src/components/Navigation/ProfileButton.jsx
@@ -88,7 +88,6 @@ function ProfileButton() {
                     modalComponent={<SignupFormModal />}
                   />
                 </div>
-
               </span>
             </div>
           )}
