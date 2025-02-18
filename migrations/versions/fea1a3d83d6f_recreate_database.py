679691b2284247b3647936a3cce86877e33e6fd0
Kai <fengkai0129@gmail.com>
2025-02-18 00:11:33 -0800

    seed

--- migrations/versions/fea1a3d83d6f_recreate_database.py
+++ migrations/versions/fea1a3d83d6f_recreate_database.py
@@ -1,8 +1,8 @@
-"""Initial migration
+"""Recreate database
 
-Revision ID: 0dab29b4f76b
+Revision ID: fea1a3d83d6f
 Revises: 
-Create Date: 2025-02-17 16:29:13.818245
+Create Date: 2025-02-18 00:09:57.600638
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0dab29b4f76b'
+revision = 'fea1a3d83d6f'
 down_revision = None
 branch_labels = None
 depends_on = None
