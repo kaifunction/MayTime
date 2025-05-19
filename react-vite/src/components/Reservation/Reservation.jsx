dd4cb86bab96cc89d1a5ed62b549611e17238be9
Kai <fengkai0129@gmail.com>
2025-05-19 10:56:39 -0700

    reservation page

--- react-vite/src/components/Reservation/Reservation.jsx
+++ react-vite/src/components/Reservation/Reservation.jsx
@@ -1,10 +1,13 @@
-function Reservation (){
-     return(
-          <h1>
-               Reservation
-          </h1>
-     )
-}
+import "./Reservation.css";
 
+function Reservation() {
+  return (
+    <div className="reservation-page-container">
+      <h1 className="reservation-title-h1">
+        <span style={{ fontWeight: "bold" }}>RESERVATION</span>
+      </h1>
+    </div>
+  );
+}
 
 export default Reservation;
