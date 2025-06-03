1526ef7c614a58144da6ef14ea454a6e29d59d65
Kai <fengkai0129@gmail.com>
2025-06-03 12:43:56 -0700

    navigation links

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -9,44 +9,19 @@ function Navigation() {
     <div className="nav-bar-container">
       <div className="nav-bar-left-logo">
         <NavLink to="/">
-          <img
-            src={MaytimeLogo}
-            alt="Logo"
-            className="nav-bar-left-logo"
-          />
+          <img src={MaytimeLogo} alt="Logo" className="nav-bar-left-logo" />
         </NavLink>
       </div>
-      <div className="nav-bar-left">
-        <div className="nav-bar-left-links">
-          {/* <NavLink to="/" className="nav-bar-button">
-            Home
-          </NavLink>
+      <div className="nav-bar-center">
+        <NavButton to="/" label="Home" />
+        <NavButton to="/photos" label="Photos" />
+        <NavButton to="/about_us" label="About Us" />
+        <NavButton to="/reservation" label="Reservation" />
+        <NavButton to="/contact_us" label="Contact Us" />
+      </div>
 
-          <NavLink to="/photos" className="nav-bar-button">
-            <div>Photos</div>
-          </NavLink>
-
-          <NavLink to="/about_us" className="nav-bar-button">
-            <div>About Us</div>
-          </NavLink>
-
-          <NavLink to="/reservation" className="nav-bar-button">
-            <div>Reservation</div>
-          </NavLink>
-
-          <NavLink to="/contact_us" className="nav-bar-button">
-            <div>Contact Us</div>
-          </NavLink> */}
-          <NavButton to="/" label="Home" />
-          <NavButton to="/photos" label="Photos" />
-          <NavButton to="/about_us" label="About Us" />
-          <NavButton to="/reservation" label="Reservation" />
-          <NavButton to="/contact_us" label="Contact Us" />
-        </div>
-
-        <div className="nav-bar-right-loginButton">
-          <ProfileButton />
-        </div>
+      <div className="nav-bar-profile">
+        <ProfileButton />
       </div>
     </div>
   );
